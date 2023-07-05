9da2fc97fa5c94c6fb328ad07bc5d3fc23e00845
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-04 22:08:15 -0500

    add: Loader Canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Kevin</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D vials, user
+            I develop 3D visuals, user
             <b className="sm:block hidden" /> interfaces and web applications.
           </p>
         </div>
