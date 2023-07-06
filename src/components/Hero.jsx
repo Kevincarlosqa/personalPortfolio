a0fac8580b3876d0d4d5d0e834cecfea15724e18
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-06 11:06:31 -0500

    fix: v11

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      {isMobile ? <ComputersCanvas /> : <EarthCanvas />}
+      {isMobile ? <EarthCanvas /> : <ComputersCanvas />}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
