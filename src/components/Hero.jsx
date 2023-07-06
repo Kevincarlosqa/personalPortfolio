8242b1b76914f798be4a6731dd7bd9d499fcd4fa
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-06 11:00:12 -0500

    fix: v9

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+// import { ComputersCanvas } from "./canvas";
+import { EarthCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -23,7 +24,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      <EarthCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
