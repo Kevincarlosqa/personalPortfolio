822cea888aadacf36ad28f76de0a7c10fdaa8a81
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-06 10:54:11 -0500

    fix: v6

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./glass/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   console.log(computer);
   return (
     <mesh>
