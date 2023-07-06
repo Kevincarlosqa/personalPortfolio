aea55346b671cc279c1acc5b8e61efd7f2d80c77
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-06 12:00:16 -0500

    fix: v15

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,11 +1,15 @@
 import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
+  const { gl } = useThree();
+
+  // Asegurarse de que el modelo se mantenga cargado en memoria
+  gl.initTextureLoader().load(earth);
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
