292657dfdf03755c5d8f4bf03d000e64d325aabd
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-06 11:46:28 -0500

    fix: v13

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
+import { isMobile } from "react-device-detect";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -129,7 +130,7 @@ const Contact = () => {
         variants={slideIn("right", "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
-        {/* <EarthCanvas /> */}
+        {isMobile ? "" : <EarthCanvas />}
       </motion.div>
     </div>
   );
